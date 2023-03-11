d3a053ffa2d9afc6d042e89b62e66e1b0ebc8f69
Sebas <jbautistas@unal.edu.co>
2023-03-10 19:32:55 -0500

    Estructura register basica

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -7,6 +7,12 @@ class RegisterController {
 	async register(req: Request, res: Response, next: NextFunction): Promise<void> {
 		res.json(await registerFacade.register()).status(STATUS_CODES.OK);
 	}
+	async update(req: Request, res: Response, next: NextFunction): Promise<void> {
+		res.json(await registerFacade.update()).status(STATUS_CODES.OK);
+	}
+	async show_page(req: Request, res: Response, next: NextFunction): Promise<void> {
+		res.json(await registerFacade.show()).status(STATUS_CODES.OK);
+	}
 }
 
 export default new RegisterController();
